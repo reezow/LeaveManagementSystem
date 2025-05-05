67761afd904f0bb0b39c1a35924e493c2dfe51d6
reezow <reeceaidanfarmer@gmail.com>
2025-05-05 13:44:06 +0200

    Created and Updated the local db

---
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementSystem.Web.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+
+        [Column(TypeName ="nvarchar(150)")]
+        public string Name { get; set; }
+
+        [Display(Name = "Number of Days")]
+        public int NumberOfDays { get; set; }
+    }
+}
